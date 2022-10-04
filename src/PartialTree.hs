147a20ffcf7eb9b1458b8a30218964b3497ea49a
Iceman <iceman@satisfiesvalues.org>
2022-10-04 12:55:38 -0400

    Attempt to restructure the PartialTreeNode ADT.

--- src/PartialTree.hs
+++ src/PartialTree.hs
@@ -14,15 +14,9 @@ import           Utils
 import qualified Data.Map      as M
 import qualified Data.Sequence as Q
 
--- OK: So v0 of this is
-
-
--- Google gives nothing. Instead, we're going to think very hard.
---
--- You receive a new root. That root:
---
--- - Will never contain the old root hash as a subtree: if something changed
---   that pushes up a new root, that root's hash has changed.
+-- A PartialTree is a partial mirror of a FullTree. PartialTree is meant to
+-- replicate as much structure as possible from a FullTree and be updatable
+-- when the root of the FullTree changes.
 
 
 -- The server side tree is a set of hashes, but the client side tree needs to
@@ -34,44 +28,39 @@ type PartialIndex k v = Index k (Hash256, PartialTreeNode k v)
 
 -- -----------------------------------------------------------------------
 
--- | The toplevel of a partially synced tree.
+-- | The toplevel of a partially synced tree. All value nodes keep track of
+-- their own Hash256.
 data PartialTreeNode k v
   -- An index node that has known holes in it.
   --
   -- When the last hole is filled, this gets switched to a Completed node.
-  = IncompleteIndex (Index k (Hash256, Subref k v)) (Hitchhikers k v)
+  = IncompleteIndex Hash256 (Index k (PartialTreeNode k v)) (Hitchhikers k v)
+
+  -- | We don't have a copy of this node. We store instead the part of the
+  -- index that covered this range in hopes of reconstituting grandchild nodes
+  -- in the future.
+  | MissingNode Hash256 (Index k (PartialTreeNode k v))
 
   -- A node which has completed synchronization.
   | Completed (CompletedSync k v)
   deriving (Show, Eq)
 
--- | Subtrees which have completed synchronization and don't hold Subrefs to
--- possibly old data.
+-- | Subtrees which have completed synchronization and don't hold references to
+-- old data.
 data CompletedSync k v
   -- | Index node that's completed synchronization. We keep the hash around in
   -- the index so we can hopefully reuse parts of the tree when we
-  = CompletedIndex (Index k (Hash256, CompletedSync k v)) (Hitchhikers k v)
+  = CompletedIndex Hash256 (Index k (CompletedSync k v)) (Hitchhikers k v)
 
   -- | All leaf nodes are complete; you either have it in its entirety or you
   -- do not.
-  | CompletedLeaf (LeafVector k v)
+  | CompletedLeaf Hash256 (LeafVector k v)
   deriving (Show, Eq)
 
--- | A reference in an IncompleteIndex.
-data Subref k v
-  -- | Everything under this hash has been completely loaded.
-  = Complete (CompletedSync k v)
+-- | The rightmost index of the node (for traversing back to it).
+type KeyLoc k = Maybe k
 
-  -- | We don't have a copy of this node. We store instead the part of the
-  -- index that previously covered.
-  | MissingNode (Index k (Hash256, Subref k v))
-
-  -- | W
-  | PartialNode (PartialTreeNode k v)
-  deriving (Show, Eq)
-
--- | A pair of the rightmost index of the node (for traversing back to it) and
--- the hash to search by content address.
+-- | A pair of a KeyLoc and the hash to search by content address.
 type FetchRequest k = (Maybe k, Hash256)
 
 -- | Inclusive range query based on key type.
@@ -83,15 +72,10 @@ data Range k
 -- -----------------------------------------------------------------------
 
 -- | Given the value of a root node, create a PartialTree to track it.
-newPartialFromRoot :: TreeNode k v -> PartialTree k v
+newPartialFromRoot :: Hash256 -> PartialTree k v
 
-newPartialFromRoot (NodeLeaf leaves) =
-  PartialTree (Just $ Completed $ CompletedLeaf leaves)
-
-newPartialFromRoot (NodeIndex (Index keys hashes) hh) =
-  PartialTree (Just $ IncompleteIndex (Index keys hashesWithRefs) hh)
-  where
-    hashesWithRefs = fmap (\h -> (h, MissingNode $ Index mempty mempty)) hashes
+newPartialFromRoot hash =
+  PartialTree (Just $ MissingNode hash $ Index mempty mempty)
 
 
 -- | Given a range (from, to), calculate all the nodes we know we'd need to
@@ -101,67 +85,66 @@ newPartialFromRoot (NodeIndex (Index keys hashes) hh) =
 ensureRange :: forall k v. Ord k
             => Range k -> PartialTree k v -> Seq (FetchRequest k)
 ensureRange _     (PartialTree Nothing)     = Empty
-ensureRange range (PartialTree (Just root)) = scan range root
+ensureRange range (PartialTree (Just root)) = scan range Nothing root
   where
-    scan :: Range k -> PartialTreeNode k v -> Seq (FetchRequest k)
-    scan _ (Completed _)               = Empty
-    scan range (IncompleteIndex idx _) =
-      join $ fmap (checkNode range) $ reqAlignedNodes $ filterNodes range idx
+    scan _     _   (Completed _)             = Empty
+    scan range loc (MissingNode hash _)      = Q.singleton (loc, hash)
+    scan range _   (IncompleteIndex _ idx _) =
+      join $ fmap (\(k,v) -> scan range k v) $ reqAlignedNodes $
+                    filterNodes range idx
 
-    -- We need to walk across the indexes that match the
-    filterNodes :: Range k -> Index k (Hash256, Subref k v)
-                -> Index k (Hash256, Subref k v)
     filterNodes (LessThanEq k)    = removeGreaterThan k
     filterNodes (Between lt gt)   = removeGreaterThan lt . removeLessThan gt
     filterNodes (GreaterThenEq k) = removeLessThan k
 
-    checkNode :: Range k -> (Maybe k, (Hash256, Subref k v))
-              -> Seq (FetchRequest k)
-    checkNode _ (_, (_, Complete _))              = Empty
-    checkNode _ (key, (hash, MissingNode _))      = Q.singleton (key, hash)
-    checkNode range (key, (hash, PartialNode pn)) = scan range pn
-
-    reqAlignedNodes :: Index k (Hash256, Subref k v)
-                    -> Seq (Maybe k, (Hash256, Subref k v))
-    reqAlignedNodes (Index keys vals) = Q.zip prepareKeys vals
-      where
-        prepareKeys = (fmap Just keys) |> Nothing
+    reqAlignedNodes (Index keys vals) = Q.zip (fmap Just keys |> Nothing) vals
 
 -- | Attempts to lookup a piece of data. Returns either a request for another
 -- node in the tree or a definitive answer of whether the key is there or not.
 lookup :: Ord k
        => k
        -> PartialTree k v
-       -> IO (Either (FetchRequest k) (Maybe v))
+       -> Either (FetchRequest k) (Maybe v)
 
-lookup key (PartialTree Nothing)     = pure $ Right $ Nothing
+lookup key (PartialTree Nothing)     = Right $ Nothing
 
-lookup key (PartialTree (Just node)) = lookupInPartialTreeNode node
+lookup key (PartialTree (Just node)) = lookupInPartial (Nothing, node)
   where
-    lookupInPartialTreeNode (Completed completedSync) =
-      lookupInCompleted completedSync
-    lookupInPartialTreeNode (IncompleteIndex idx hh) =
-      lookupInIncomplete idx hh
-
-    lookupInSubref _ _ (Complete cs)        = lookupInCompleted cs
-    lookupInSubref key hash (MissingNode _) = pure $ Left $ (key, hash)
-    lookupInSubref _ _ (PartialNode ptn)    = lookupInPartialTreeNode ptn
-
-    lookupInIncomplete idx hitchhikers =
+    lookupInPartial (_, (IncompleteIndex hash idx hitchhikers)) =
       case findInHitchhikers key hitchhikers of
-        Just v  -> pure $ Right $ Just v
-        Nothing ->
-          let (closestKey, (hash, subref)) = findSubnodeByKey key idx
-          in lookupInSubref closestKey hash subref
+        Just v  -> Right $ Just v
+        Nothing -> lookupInPartial $ findSubnodeByKey key idx
 
-    lookupInCompleted (CompletedIndex idx hitchhikers) =
+    lookupInPartial (loc, (MissingNode hash idx)) = Left $ (loc, hash)
+
+    lookupInPartial (_, (Completed completed)) = lookupInCompleted completed
+
+    lookupInCompleted (CompletedIndex _ idx hitchhikers) =
       case findInHitchhikers key hitchhikers of
-        Just v  -> pure $ Right $ Just v
-        Nothing -> let (_, (_, child)) = findSubnodeByKey key idx in
-          lookupInCompleted child
-    lookupInCompleted (CompletedLeaf leaves) =
-      pure $ Right $ findInLeaves key leaves
+        Just v  -> Right $ Just v
+        Nothing -> lookupInCompleted $ snd $ findSubnodeByKey key idx
 
+    lookupInCompleted (CompletedLeaf _ leaves) =
+      Right $ findInLeaves key leaves
+
+{-
+
+-- | We have received a piece requested. We now insert that back into the
+-- structure, reconstituting as much of the tree as we can from
+fetched :: FetchRequest k
+        -> TreeNode k v
+        -> PartialTreeNode k v
+        -> PartialTreeNode k v
+fetched (k, hash) fetched tree = findTarget tree
+  -- What does this do? First we must navigate downward to the place in the
+  -- tree where we need to perform the completion, then we need to
+  where
+    -- Navigate through the tree to the node containing the MissingNode subref.
+    findTarget tree = undefined
+
+-- TODO: Am I struggling because the data structures are wrong? The mix of
+
+-}
 
 -- -----------------------------------------------------------------------
 
@@ -186,15 +169,3 @@ replaceRoot = go (Index mempty mempty)
     indexesSame (Index _ p) (Index _ prevHashes) = prevHashes == fmap fst p
 -}
 
-
--- | We have received a piece requested. We now insert that back into the
--- structure, reconstituting
-fetched :: FetchRequest k
-        -> TreeNode k v
-        -> PartialTreeNode k v
-        -> PartialTreeNode k v
-fetched (k, hash) fetched tree =
-  -- What does this do? First we must navigate downward to the place in the
-  -- tree where we need to perform the completion, then we need to
-
-  undefined
