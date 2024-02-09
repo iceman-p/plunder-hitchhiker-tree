aa410824456e017f3ed76fcc998754dd7157b49c
Iceman <iceman@satisfiesvalues.org>
2024-02-09 12:24:06 -0500

    Implement restrict keys.

--- src/HitchhikerSetMap.hs
+++ src/HitchhikerSetMap.hs
@@ -5,6 +5,8 @@ import           ClassyPrelude
 import           Data.Map      (Map)
 import           Data.Set      (Set)
 
+import           Safe          (tailSafe)
+
 import           Impl.Index
 import           Impl.Leaf
 import           Impl.Tree
@@ -18,9 +20,25 @@ import qualified HitchhikerSet as HS
 
 import qualified Data.Map      as M
 import qualified Data.Set      as S
+import qualified Data.Vector   as V
 
 empty :: TreeConfig -> HitchhikerSetMap k v
 empty config = HITCHHIKERSETMAP config Nothing
+
+-- Takes a list of leaves and make a valid
+fromLeafMaps :: (Show k, Show v, Ord k, Ord v)
+             => TreeConfig
+             -> [Map k (NakedHitchhikerSet v)]
+             -> HitchhikerSetMap k v
+fromLeafMaps config []   = HitchhikerSetMap.empty config
+fromLeafMaps config [m]  = HITCHHIKERSETMAP config $ Just
+                         $ HitchhikerSetMapNodeLeaf m
+fromLeafMaps config rawMaps = HITCHHIKERSETMAP config $ Just node
+  where
+    node = fixUp config (hhSetMapTF config) treeIndex
+    treeIndex = indexFromList idxV vals
+    idxV = V.fromList $ tailSafe $ map (fst . M.findMin) rawMaps
+    vals = V.fromList $ map HitchhikerSetMapNodeLeaf rawMaps
 
 -- SetMap is the complicated one, because we use two different representations:
 -- one for the leafs (where we use HitchhikerSets to collect the mass keys) and
@@ -195,3 +213,75 @@ lookup key (HITCHHIKERSETMAP config (Just top)) = lookInNode mempty top
     buildSetFrom leaves hh = case M.lookup key leaves of
       Nothing  -> HS.fromArraySet config hh
       Just ret -> HS.insertMany hh (weave config ret)
+
+-- TODO: This isn't the best performing implementation, but it's Good Enough
+-- for now. This is an attempt at using something like the
+-- MultiIntesersectV3Naive code in a different context. This should be good
+-- enough at "small" (<100K) items, and will probably be faster on the naive
+-- tree walking interpreter than a proper tree pruning implementation for the
+-- same reason V6Stack was: function frames are super expensive.
+--
+restrictKeys :: forall k v
+              . (Show k, Show v, Ord k, Ord v)
+             => HitchhikerSet k
+             -> HitchhikerSetMap k v
+             -> HitchhikerSetMap k v
+restrictKeys _ orig@(HITCHHIKERSETMAP _ Nothing) = orig
+
+restrictKeys (HITCHHIKERSET _ Nothing) (HITCHHIKERSETMAP config _) =
+  HitchhikerSetMap.empty config
+
+restrictKeys (HITCHHIKERSET sConfig (Just a))
+             (HITCHHIKERSETMAP mConfig (Just b)) =
+  fromLeafMaps mConfig $ setlistMaplistIntersect [] as bs
+  where
+    as = getLeafList HS.hhSetTF a
+    bs = getLeafList (hhSetMapTF mConfig) b
+
+-- We walk the as and bs list in parallel. When the current a overlaps with
+-- the current b, we put a into a list of partial mapping sets and keep
+-- accumulating those until we don't have a
+setlistMaplistIntersect :: forall k v
+         . (Show k, Ord k, Show v)
+        => [ArraySet k]
+        -> [ArraySet k]
+        -> [Map k v]
+        -> [Map k v]
+setlistMaplistIntersect _ a []                = []
+setlistMaplistIntersect [] [] b                = []
+setlistMaplistIntersect partial ao@(a:as) bo@(b:bs) =
+    let aMin = ssetFindMin a
+        aMax = ssetFindMax a
+        bMin = fst $ M.findMin b
+        bMax = fst $ M.findMax b
+        overlap = aMin <= bMax && bMin <= aMax
+
+        filteredBy s rest = let f = M.restrictKeys b s
+                            in if M.null f then rest
+                                           else f:rest
+
+        toSet :: [ArraySet k] -> Set k
+        toSet = foldl' S.union S.empty . map (S.fromList . ssetToAscList)
+
+    in case (partial, overlap) of
+         ([], False)
+           | aMax > bMax -> setlistMaplistIntersect [] ao bs
+           | otherwise   -> setlistMaplistIntersect [] as bo
+         (partial, False) -> error "Should be impossible"
+         (partial, True)
+           | aMax == bMax ->
+               filteredBy (toSet (a:partial)) $ setlistMaplistIntersect [] as bs
+           | aMax < bMax ->
+               setlistMaplistIntersect (a:partial) as bo
+           | otherwise ->
+               filteredBy (toSet (a:partial)) $ setlistMaplistIntersect [] ao bs
+
+a = setlistMaplistIntersect [] [ssetFromList [1, 2], ssetFromList [3, 4]] [M.fromList [(1, "a"), (2, "b"), (3, "c"), (4, "d")]]
+
+b = setlistMaplistIntersect [] [ssetFromList [1, 2], ssetFromList [3, 5], ssetFromList [6]]
+            [M.fromList [(1, "a"), (2, "b"), (3, "c")],
+             M.fromList [(4, "d"), (5, "e"), (6, "f")]]
+
+
+
+-- Dang. Now to debug the above pile of garbage.
